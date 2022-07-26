8ea86f9955aca9e47fd2d98dc4225c5a889bb39e
Guillaume Aubert <guillaume@aubert.ws>
2022-07-26 15:06:49 +0200

    v->hit

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: gaubert <gaubert@student.42lausanne.ch>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/18 15:03:08 by gaubert           #+#    #+#             */
-/*   Updated: 2022/07/18 15:04:47 by gaubert          ###   ########.fr       */
+/*   Updated: 2022/07/19 13:40:18 by gaubert          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,20 @@ void	while_dof(t_game *g, t_rvars *v)
 		v->mp = v->my * g->map_width + v->mx;
 		if (v->mp > 0 && v->mp < g->map_width * g->map_height
 			&& g->map[v->mp] == '1')
+		{
 			v->dof = 8;
+		}
 		else
 		{
 			v->rx += v->xo;
 			v->ry += v->yo;
 			v->dof += 1;
 		}
+		if (v->mp > 0 && v->mp < g->map_width * g->map_height)
+			v->hit = g->map[v->mp];
 	}
+	if (v->hit == 'N' || v->hit == 'W' || v->hit == 'E' || v->hit == 'S')
+		v->hit = '0';
 }
 
 float	dist(float ax, float ay, float bx, float by)
